0f57a9ced077154cf3ee23d909d564690eb649b3
Rafael <msmk.swimmer@gmail.com>
2023-03-19 00:55:36 +0300

    add ci in .github and filled out schema.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'users#index'
+
   devise_for :users
+
   resources :posts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
