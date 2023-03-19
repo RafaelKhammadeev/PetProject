15c990aa811c2d453e867bdbe788ade4d7657101
Rafael <msmk.swimmer@gmail.com>
2023-03-19 22:11:38 +0300

    fix bag with signout

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   root to: 'users#index'
   devise_for :users
 
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-
   resources :posts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
