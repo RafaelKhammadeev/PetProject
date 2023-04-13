5a14c5b9f5dc748357eed42223222c510dd6d3b1
RafaelKhammadeev <94076749+RafaelKhammadeev@users.noreply.github.com>
2023-04-13 13:00:30 +0300

    Work with user controller and view (#4)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,6 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.fetch(:user, {})
+    params.require(:user).permit(:name, :age)
   end
 end
