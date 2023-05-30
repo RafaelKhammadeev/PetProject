32049177ed2be545f439606c32ff0e1f54763fe1
Rafael <msmk.swimmer@gmail.com>
2023-05-30 20:50:13 +0300

    destroy namespace and rubocop_todo.yml

--- app/policies/users/post_policy.rb
+++ app/policies/users/post_policy.rb
@@ -1,28 +1,26 @@
-module Users
-  class PostPolicy < ApplicationPolicy
-    authorize :user, allow_nil: true
+class PostPolicy < ApplicationPolicy
+  authorize :user, allow_nil: true
 
-    def index?
-      user.present?
-    end
-
-    def show?
-      true
-    end
-
-    def new?
-      user.present?
-    end
-
-    def edit?
-      true if record&.user_id == user.id
-    end
-
-    def create?
-      new?
-    end
-
-    alias update? edit?
-    alias destroy? update?
+  def index?
+    user.present?
   end
+
+  def show?
+    user.present?
+  end
+
+  def new?
+    user.present?
+  end
+
+  def edit?
+    true if record&.user_id == user.id
+  end
+
+  def create?
+    new?
+  end
+
+  alias update? edit?
+  alias destroy? update?
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   root to: 'users#index'
   devise_for :users
 
-  get '/posts', to: 'posts#index'
-
   resources :users, only: %i[index show destroy] do
-    namespace :users do
-      resources :posts
-    end
+    resources :posts
   end
 end
