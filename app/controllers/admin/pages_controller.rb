03ef5ae47622302553253ee2ab2e109696eb3aae
Rafael <msmk.swimmer@gmail.com>
2023-07-03 16:07:11 +0300

    destroy contact message

--- app/controllers/admin/pages_controller.rb
+++
@@ -1,7 +0,0 @@
-module Admin
-  class PagesController < ApplicationController
-    def support
-      @contact_messages = Admin::ContactMessage.all
-    end
-  end
-end
--- app/models/admin/contact_message.rb
+++
@@ -1,8 +0,0 @@
-module Admin
-  class ContactMessage < ApplicationRecord
-    belongs_to :user
-
-    validates :title, presence: true, length: { minimum: 5, maximum: 100 }
-    validates :message, presence: true, length: { minimum: 10, maximum: 500 }
-  end
-end
