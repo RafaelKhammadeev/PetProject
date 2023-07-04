dc0881dc4263a7f86f778819a4da763fbd0c3263
Rafael <msmk.swimmer@gmail.com>
2023-07-04 11:36:28 +0300

    add create delete for comment and styled

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,39 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_comment, only: %i[destroy]
+  before_action :set_post, only: %i[create destroy]
+  before_action :set_user, only: %i[create destroy]
+
+  def create
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to user_post_path(@user, @post), notice: 'Comment was successfully created.'
+    else
+      render user_post_path(@user, @post), status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to user_post_path(@user, @post), notice: 'Comment was successfully destroyed.'
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comments = @post.comments.order(created_at: :desc)
   end
 
   def new
