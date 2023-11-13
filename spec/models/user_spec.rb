8294b3ca93518f2f1195009b9e9da96fee24690b
RafaelKhammadeev <94076749+RafaelKhammadeev@users.noreply.github.com>
2023-11-13 12:24:38 +0300

    Update spec/models/user_spec.rb
    Co-authored-by: Denis Zakharov <78980031+DenisZackharov@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 describe User do
+  let(:user) { build :user, name: name, surname: surname }
+  let(:name) { "John" }
+  let(:surname) { "Johns" }
+
   it 'valid user' do
     expect(build(:user)).to be_valid
   end
 
-  describe '#invalid user' do
-    it 'without a name' do
-      expect(build(:user, name: '')).to_not be_valid
+  describe '#valid?' do
+    context "without name" do
+      let(:name) { nil }
+      it 'try to build user' do
+        expect(user.valid?).to_be falsey
+      end
     end
 
     it 'without a surname' do
